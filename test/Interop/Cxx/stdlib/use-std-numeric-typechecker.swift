5866d46f5c1811d98acc783219b184b056a50fd8
Egor Zhdan <e_zhdan@apple.com>
2024-10-02 16:05:37 +0100

    [cxx-interop] Modularize __msvc_bit_utils on Windows
    `__msvc_bit_utils.hpp` was added in a recent version of MSVC, and it is causing build errors for SwiftCompilerSources:
    ```
    C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\include\numeric:598:12: error: function '_Select_countr_zero_impl<unsigned long long, (lambda at C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\include\numeric:598:55)>' with deduced return type cannot be used before it is defined
        return _Select_countr_zero_impl<_Common_unsigned>([=](auto _Countr_zero_impl) {
    ```
    
    This change references the `__msvc_bit_utils.hpp` header from the modulemap. Since we still need to support older versions of Visual Studio that do not provide `__msvc_bit_utils.hpp`, this also teaches ClangImporter to inject an empty header file named `__msvc_bit_utils.hpp` into the system include directory, unless it already exists.
    
    rdar://137066642

---
+++ test/Interop/Cxx/stdlib/use-std-numeric-typechecker.swift
@@ -0,0 +1,7 @@
+// RUN: %target-typecheck-verify-swift -I %S/Inputs -cxx-interoperability-mode=upcoming-swift -Xcc -std=c++17
+// RUN: %target-typecheck-verify-swift -I %S/Inputs -cxx-interoperability-mode=upcoming-swift -Xcc -std=c++20
+
+import StdPair
+import CxxStdlib
+
+let _ = getGCD(12, 15)
